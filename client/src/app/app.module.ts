0a79207ece9a07d78d11cc7f88d4211e6bd42ca4
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-08 19:09:57 -0500

    Replaced basic template

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollectionComponent } from './collection/collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
